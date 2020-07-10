88c50a43184a2592d59c2b7807a9f3b16bc04fb2
meloegel <meloegel@gmail.com>
2020-07-10 12:35:38 -0400

    updates for deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const projectsRouter = require('./data/seeds/projectsRouter');
@@ -8,12 +10,15 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.send(`Its working!`)
+    const message = process.env.MESSAGE
+    res.status(200).json({ message })
 });
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/projects/:id/actions', actionsRouter);
 
-server.listen(8000, () => {
-    console.log('\n*** Server Running on http://localhost:8000 ***\n');
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 })
